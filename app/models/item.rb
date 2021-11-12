bbaa89ad2623a4f9cfbc509315c87765c8066e1b
suwonkun <92506153+suwonkun@users.noreply.github.com>
2021-11-12 14:17:28 +0900

    商品一覧表示機能の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,7 +6,7 @@ class Item < ApplicationRecord
   belongs_to :derivery_charge
   belongs_to :days_to_ship
   belongs_to :user
-  # has_one :buy
+  has_one :buy
   has_one_attached :image
 
   validates :item_name, presence: true
