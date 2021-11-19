b7265374eba4251ecb365f119224f547e56efd4d
suwonkun <92506153+suwonkun@users.noreply.github.com>
2021-11-19 10:24:36 +0900

    修正

--- app/models/days_to_ship.rb
+++ app/models/days_to_ship.rb
@@ -1,8 +1,9 @@
 class  DaysToShip< ActiveHash::Base
   self.data = [
        {id: 0, days_to_ship: '---'},
-       {id: 1, days_to_ship: '着払い（購入者負担）'},
-       {id: 2, days_to_ship: '送料込み（出品者負担）'}, 
+       {id: 1, days_to_ship: '1~2日で発送'},
+       {id: 2, days_to_ship: '2~3日で発送'}, 
+       {id: 3, days_to_ship: '4~7日で発送'}, 
     ]
   include ActiveHash::Associations
   has_many :items
